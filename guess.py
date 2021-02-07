1ec428de65737164dae5a2246e66869352659ab6
Thomas Williams <tefwilliams@icloud.com>
2021-02-07 11:19:42 +0000

    Add GuessType enum

--- guess.py
+++ guess.py
@@ -1,21 +1,32 @@
 
 from __future__ import annotations
 from word import Word
+from enum import Enum
 
 
 class Guess(Word):
     def __init__(self: Guess, guess: str, word: Word) -> None:
         super().__init__(guess)
-        self.word = word
+        
+        self.__word = word
+        self.__type = self.__get_type()
 
+    def __get_type(self: Guess) -> GuessType:
         assert self.isalpha()
-        assert self.is_letter or self.is_word
 
+        if self.length == 1:
+            return GuessType.letter
+
+        elif self.length == self.__word.length:
+            return GuessType.word
+
+        raise ValueError
+        
     @property
     def is_letter(self: Guess) -> bool:
-        return self.length == 1
+        return self.__type == GuessType.letter
 
-    @property
-    def is_word(self: Guess) -> bool:
-        return self.length == self.word.length
 
+class GuessType(Enum):
+    letter = 1
+    word = 2
